f82b7b2f8a8a893b3ac5b45f199a8e3a6873bcda
DESKTOP-8UOCMFI\Gautam <gautammobi999@gmail.com>
2020-11-23 09:02:34 +0530

    2311_updates

--- app/Http/Resources/User/UserResource.php
+++ app/Http/Resources/User/UserResource.php
@@ -10,23 +10,20 @@ class UserResource extends JsonResource
     {
         return [
             'id' => $this->id,
-            'FullName' => $this->FullName,
+            'name' => $this->name,
             'email' => $this->email,
-            'UserName_verified_at' => $this->UserName_verified_at,
-            'DateOfBirth' => $this->DateOfBirth,
-            'ImageUrl' => $this->ImageUrl,
-            'ContactNumber' => $this->ContactNumber,
-            'Address' => $this->Address,
-            'CreatedDate' => $this->CreatedDate,
-            'IsActive' => $this->IsActive,
+            'dateOfBirth' => $this->dateOfBirth,
+            'imageUrl' => $this->imageUrl,
+            'contactNumber' => $this->contactNumber,
+            'address' => $this->address,
+            'createdDate' => $this->createdDate,
+            'isActive' => $this->isActive,
             'region_Id' => $this->regions_Id,
             'role_Id' => $this->role_Id,
             'gender_Id' => $this->genders_Id,
             'region' => $this->region,
             'gender' => $this->genders,
             'role' => $this->role,
-            'actionToRoles' => $this->role->actionToRoles ?? 0,
-
         ];
     }
 }
